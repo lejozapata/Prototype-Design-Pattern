19d3b60fcdbca44a5947c0df838cc6dc6dc70e71
lejozapata <36249988+lejozapata@users.noreply.github.com>
2020-03-03 21:05:29 -0500

    Yolo
    New Items

---
+++ Prototype/Characters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+
+    abstract class CharacterPrototype
+    {
+        public abstract CharacterPrototype Clone();
+    }
+
+    class Character : CharacterPrototype
+    {
+        private string Name;
+        private int Life;
+        private string Category;
+        private string Weapon;
+        private string Pet;
+        //private Object UltimateMovement;
+
+        public Character(string name, int life, string category, string weapon,string pet)
+        {
+            Name = name;
+            Life = life;
+            Category = category;
+            Weapon = weapon;
+            Pet = pet;
+            //UltimateMovement = ultimateMovement;
+        }
+
+        public override CharacterPrototype Clone()
+        {
+            string skillsList = GetSkills();
+            Console.WriteLine("Cloning character with skills: {0}", skillsList.Remove(skillsList.LastIndexOf(",")));
+
+            return MemberwiseClone() as CharacterPrototype;
+        }
+
+        private string GetSkills()
+        {
+            var skills = "";
+            if (!string.IsNullOrWhiteSpace(Name))
+            {
+                skills += Name + ", ";
+            }
+            if (!string.IsNullOrWhiteSpace(Life.ToString()))
+            {
+                skills += Life.ToString() + ", ";
+            }
+            if (!string.IsNullOrWhiteSpace(Category))
+            {
+                skills += Category + ", ";
+            }
+            if (!string.IsNullOrWhiteSpace(Weapon))
+            {
+                skills += Weapon + ", ";
+            }
+            if (!string.IsNullOrWhiteSpace(Pet))
+            {
+                skills += Pet + ", ";
+            }
+            //if (!string.IsNullOrWhiteSpace(UltimateMovement))
+            //{
+            //    skills += UltimateMovement + ", ";
+            //}
+            
+            return skills;
+        }
+    }
+
+    class Army
+    {
+        private Dictionary<string, CharacterPrototype> _characters = new Dictionary<string, CharacterPrototype>();
+
+        public CharacterPrototype this[string name]
+        {
+            get { return _characters[name]; }
+            set { _characters.Add(name, value); }
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Army army = new Army();
+
+            // Initialize with default characters
+            army["Archer"] = new Character("Archer",200,"Distance","Bow","Horse");
+            army["Knight"] = new Character("Knight", 300, "Melee", "Sword", "Dog");
+            army["Wizard"] = new Character("Wizard", 500, "Distance", "crosier", "Eagle");
+            army["Demon"] = new Character("Demon", 400, "Melee", "Claws", "Beast");
+            army["Robot"] = new Character("Robot", 1000, "Distance", "Lazer", "AI");
+
+            // Now we can clone the characters.
+            Character Archer1 = army["Archer"].Clone() as Character;
+            Character Robot1 = army["Robot"].Clone() as Character;
+            Character Wizard1 = army["Wizard"].Clone() as Character;
+
+
+            Console.ReadKey();
+        }
+    }
+}
